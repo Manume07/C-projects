1defc4081e1a2e869e74ae5f0f44f078e10b2214
Manuel Campana <MANUEL.CAMPANA@local.ittcesena.it>
2025-10-13 08:53:50 +0200

    prova

---
+++ trying/.vs/CopilotSnapshots/084C6F166E846C418D0F8D3E159BABAA/DA208F7AEB36A64990E3F6027D87F1BD/EDDA32B2262CCC170ABF8ACD0D3D0F3F
@@ -0,0 +1,27 @@
+﻿namespace ContoBancario
+{
+    internal class Program
+    {
+        static void Main()
+        {
+
+            Console.WriteLine("Inserisci il nome del titolare: ");
+            string nomeTitolare = Console.ReadLine();
+
+            Console.WriteLine("Inserisci il saldo del conto bancario");
+            double saldoConto;
+            while (true)
+            {
+                string input = Console.ReadLine();
+                bool corretto = double.TryParse(input, out saldoConto);
+                if (corretto && saldoConto > 0)
+                {
+                    break;
+                }
+                Console.WriteLine("Il saldo del conto bancario non può essere minore o uguale a 0. Inserisci un valore valido:");
+            }
+            
+
+        }
+    }
+}
---
+++ trying/Class1.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ContoBancario
+{
+    internal class ContoBancario
+    {
+        private long _numeroUnivoco;
+        private string _nomeTitolare;
+        private double _saldoConto;
+        private double _versamento;
+
+
+        public ContoBancario(long numeroUnivoco, string nomeTitolare, double saldoConto, double versamento, double prelievo)
+        {
+            _numeroUnivoco = numeroUnivoco;
+            _nomeTitolare = nomeTitolare;
+            _saldoConto = saldoConto;
+        }
+
+        public long GeneraNumeroUnivoco(long numeroUnivoco)
+        {
+            Random rnd = new Random();
+            _numeroUnivoco = rnd.NextInt64(0000000000, 9999999999);
+            return _numeroUnivoco;
+        }
+
+        public string setNomeTitolare(string nomeTitolare)
+        {
+            string nome = nomeTitolare;
+            _nomeTitolare = nome.ToLower();
+            return _nomeTitolare;
+        }
+
+        public double setRecuperaSaldo()
+        {
+            return _saldoConto;
+        }
+
+        public bool prelievoConto(double importoPrelievo)
+        {
+            if (_saldoConto <= 0)
+            {
+                return false;
+            }
+            else
+            {
+                if (importoPrelievo <= _saldoConto)
+                {
+                    _saldoConto -= importoPrelievo;
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+
+            }
+        }
+
+        public double versamentoConto(double importoVersamento)
+        {
+            bool valore;
+            valore = false;
+            while (valore == false)
+            {
+                if (_versamento > 0.0)
+                {
+                    valore = true;
+                    _saldoConto += importoVersamento;
+                }
+            }
+            return _saldoConto;
+
+
+        }
+
+       
+    }
+}
+
+
+/*conto bancario(TERMINI IN ITALIANO) CLASSI E OGGETTI
+- numero univoco 10 cifre che identifica conto (codice)
+- stringa coi nomi dei titolari (nome persona conto corrente)
+- possibile recuperare il saldo (sapere i soldi del conto)
+- il conto accetta prelievi e versamenti (puoi aggiungere e togliere soldi)
+- saldo iniziale positivo (soldi>-1)
+- prelievi non possono risultare in saldo negativo (saldo sempre positivo dopo prelievo)
+*/

c14df4b7f72ff840a28f0782a3ca21411d5821ea
Manuel Campana <MANUEL.CAMPANA@local.ittcesena.it>
2025-10-24 09:37:42 +0200

    R0.1: Completata la classe reader

--- gitignore/Reader.cs
+++ gitignore/Reader.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +9,31 @@ namespace gitignore
 {
     internal class Reader
     {
+        private string filePath;
+
+        public Reader(string path)
+        {
+            filePath = path;
+        }
+        public void LeggiFile()
+        {
+            try
+            {
+                using(StreamReader sr = new StreamReader(filePath))
+                {
+                    string line;
+
+                    while((line = sr.ReadLine()) != null)
+                    {
+                        Console.WriteLine(line);
+                    }
+                }
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine("Errore nella lettura del file: " + e.Message);
+            }
+
+        }
     }
 }

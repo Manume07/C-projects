7d51822c1eec2b194dea9d30db7531b8188208fa
Manuel Campana <MANUEL.CAMPANA@local.ittcesena.it>
2025-10-24 09:24:39 +0200

    R0.1: Completata la classe Writer

--- gitignore/Writer.cs
+++ gitignore/Writer.cs
@@ -9,6 +9,33 @@ namespace gitignore
 { 
     internal class Writer
     {
-        
+        private string filePath;
+
+        public Writer(string path)
+        {
+            filePath = path;
+        }
+
+        public void ScriviFile()
+        {
+            try
+            {
+                using(StreamWriter writer = new StreamWriter(filePath))
+                {
+                    writer.WriteLine("Prima riga");
+                    writer.WriteLine("Seconda riga");
+                    writer.WriteLine("Terza riga");
+                }
+
+                Console.WriteLine("File scritto con successo.");
+            }
+            catch (Exception ex)
+            {
+                 Console.WriteLine("Errore nellascrittura del file: " + ex.Message);
+            }
+        }
+
+
+
     }
 }
